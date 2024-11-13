bf16bfac40bd795ace050a7389507775fe4aa66f
Diego Reis <reisdiego144@gmail.com>
2024-11-12 23:42:10 -0300

    fix: search was not been putted in the searches map

--- src/main/java/com/axreng/backend/CrawlerService.java
+++ src/main/java/com/axreng/backend/CrawlerService.java
@@ -1,8 +1,7 @@
 package com.axreng.backend;
 
-import java.net.URL;
+import java.net.URI;
 import java.util.Map;
-import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -14,9 +13,9 @@ public class CrawlerService {
   private final Map<String, SearchResultDTO> searches = new ConcurrentHashMap<>();
   private final ExecutorService executor = Executors.newCachedThreadPool();
 
-  public String startSearch(String keyword, URL baseUrl) {
+  public String startSearch(String keyword, URI baseUrl) {
     String id = this.generateId(keyword, baseUrl);
-
+    searches.put(id, new SearchResultDTO(id, keyword));
     executor.submit(new SearchTask(id, keyword, baseUrl, searches));
     return id;
   }
@@ -25,7 +24,7 @@ public class CrawlerService {
     return searches.get(id);
   }
 
-  private String generateId(String keyword, URL baseUrl) {
+  private String generateId(String keyword, URI baseUrl) {
     return DigestUtils.generateHash(keyword + baseUrl.toString()).substring(0, 8);
   }
 }
